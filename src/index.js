e810dc4e2d44ed49bd5eefe3a82f11abbd19940b
Julio Denis <julio_denis@icloud.com>
2019-09-10 12:25:47 -0500

    mongo connected

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
-const express = require('express');
+const express = require("express");
+const path = require('path');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+//connecting to db
+mongoose.connect('mongodb://localhost/crud-mongo', { useNewUrlParser: true, useUnifiedTopology: true },)
+    .then(db => console.log('DB connected'))
+    .catch(err => console.log(err));
+ 
+
+
+
+
+// Importing routes
+const indexRoutes = require('./routes/index');
+
 
 const app = express();
 // Settings
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views')); //indica la ruta de la carpeta views
+app.set('view enigne', 'ejs');
 
 // Middlewares
-
-
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false})) // entiende los request html del cliente 
 // Routes
-
-
+app.use('/', indexRoutes);
 // Static Files
 
-
 // Start the server
-app.listen(app.get('port'), () => {
-    console.log(`Server on port ${app.get('port')}`);
-    
+app.listen(app.get("port"), () => {
+  console.log(`Server on port ${app.get("port")}`);
+
+  
 });
