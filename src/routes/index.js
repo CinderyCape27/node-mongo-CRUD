173a3b7fc90ee49ebd5978942d9f457a44316dec
Julio Denis <julio_denis@icloud.com>
2019-09-11 09:55:29 -0500

    primeras pruebas post

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,5 +5,10 @@ router.get('/', (req, res) => {
     res.render('index')
 })
 
+router.post('/add', (req, res) => {
+    console.log(req.body);
+    res.render('add')
+    
+})
 
 module.exports = router;
